a01f45c51ba9d197596636864d2b6d16ee78fe8d
Luis Carlos <trujillogarcialuiscarlos@gmail.com>
2025-02-08 11:19:17 -0500

    Update comand.py

--- comand.py
+++ comand.py
@@ -45,4 +45,27 @@ async def que_tal(ctx):
 async def question(ctx):
     await ctx.send(f'¿Te gustaría ver "Batman", {ctx.author.name}?')
 
+@bot.command()
+async def mem(ctx):
+    lista = os.listdir("images")
+    img_name = random.choice(lista)
+    with open(f'images/{img_name}','rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
 bot.run('Token')
